3e41e4dcb46d9f2991efc56c8f3e6c9040aba3a3
phoebehlam <62526368+phoebehlam@users.noreply.github.com>
2024-02-22 16:16:38 -0500

    Initial commit

---
+++ R/yuxi_sleeplog.R
@@ -0,0 +1,122 @@
+#'@importFrom magrittr "%>%"
+#'@export
+yuxi_sleeplog <- function(path, id, first) {
+  
+  
+  log <- data.frame(matrix(ncol = 1, nrow = 1))
+  saveRDS(log, paste(path, "sleeplog.rds", sep="/"))
+  
+  # remind yuxi that it's not recursive
+  filenames = intersect(list.files(path = path, full.names = T, recursive = F), 
+                        list.files(path = path, pattern = ".csv", full.names = T, recursive = F))
+  
+  # troubleshoot
+  # file <- read.csv("/Users/phoebelam/Desktop/yuxi/Dissertation-Morning Day 1_February 22, 2024_13.26.csv" )
+  # 
+  # file %>% 
+  #   dplyr::filter (EMAIL == 'test1') %>%
+  #   dplyr::rename (qid = ExternalReference) %>%
+  #   dplyr::mutate(qualtrics_day = daynum) %>%
+  #   dplyr::select (., qualtrics_day, StartDate, EndDate, qid, BedTime.1_1:WakeTime.3_1) -> file
+
+  
+  # loop
+  for (f in filenames) {
+    
+    print (f)
+    
+    file<-read.csv(f)
+    
+    # remind yuxi to not change the name of the survey ever
+    basename(f) %>%
+      gsub ("Dissertation-Morning Day |_", " ", .) %>%
+      substr(., 1, 3) -> daynum
+    
+    # grab id - need to tell yuxi about external reference + do not change the name of the ID column after this
+    file %>% 
+      dplyr::rename (qid = ExternalReference) %>%
+      dplyr::mutate(qualtrics_day = daynum) %>%
+      dplyr::select (., qualtrics_day, StartDate, EndDate, qid, BedTime.1_1:WakeTime.3_1) %>%
+      dplyr::filter (qid == id)-> file
+    
+    # grab bed time and wake time
+    file %>%
+      mutate(bedtime = paste(paste(BedTime.1_1, BedTime.2_1, sep=":"), BedTime.3_1, sep=" "),
+             waketime = paste(paste(WakeTime.1_1, WakeTime.2_1, sep=":"), WakeTime.3_1, sep=" ")) -> file
+  
+    # grab reference date (remind yuxi this is only correct if no binged)
+    # remind yuxi do not open the .csv from the zip
+    file %>% tidyr::separate (EndDate, c("eDate", "eTime"), " ", fill = "right", remove= FALSE) %>%
+      mutate(date = as.Date(eDate, "%Y-%m-%d")-1) %>%
+      rename(reportdate = eDate) -> file
+    
+    # trim
+    file %>% select(., qid, qualtrics_day, date, bedtime, waketime, reportdate) -> file
+    
+    # consolidate
+    log <- readRDS (paste(path, "/sleeplog.rds", sep=""))
+    log <- suppressWarnings(gtools::smartbind (log, file))
+    saveRDS (log, paste(path, "/sleeplog.rds", sep=""))
+    
+    
+  }
+  
+  log <- readRDS (paste(path, "/sleeplog.rds", sep="")) [-1,-1]
+  
+  if (nrow (log) > 0) {
+    
+    
+    #compliance alert
+    # first meaning the first date that are supposed to do MORNING diary
+    should <- c(as.Date(first), as.Date(first)+1, as.Date(first)+2, as.Date(first)+3, as.Date(first)+4, as.Date(first)+5, 
+    as.Date(first)+6, as.Date(first)+7, as.Date(first)+8, as.Date(first)+9)
+    
+    data.frame(qualtrics_day = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
+               shoulddate = should) -> should
+    
+    log %>% dplyr::mutate(qualtrics_day = as.numeric(qualtrics_day)) -> log
+    merge(log, should, by = 'qualtrics_day', all=T) -> log
+    
+    log %>%
+      dplyr::mutate_at(vars(shoulddate, reportdate),
+                       list(~as.Date(.))) %>% 
+      dplyr::mutate(compliance = case_when(shoulddate-reportdate==0~ 'ok',
+                                           shoulddate-reportdate>0~ 'late',
+                                           is.na(shoulddate-reportdate)==T~ 'missing')) -> log
+    
+    #binging alert
+    #binge defined by two entries indexing the same sleep date
+    log %>%
+      dplyr::mutate(reportdate_diff = reportdate - dplyr::lag(reportdate),
+                    reportdate_diff2 = reportdate - dplyr::lead(reportdate)) %>%
+      dplyr::mutate(binge = dplyr::case_when(reportdate_diff == 0 | reportdate_diff2 == 0~ 'binge',
+                                             is.na(reportdate_diff)==T & is.na(reportdate_diff2)==T~ NA_character_,
+                                             TRUE~ 'ok')) -> log
+    
+    # rename for output purpose
+    log %>%
+      dplyr::rename(id = qid,
+             `qualtrics diary day` = qualtrics_day,
+             `alleged sleep date` = date,
+             `date reported sleep` = reportdate,
+             `date should have reported sleep`= shoulddate) %>%
+      dplyr::select(-c(reportdate_diff, reportdate_diff2))-> log
+    
+    # output
+    openxlsx::write.xlsx(log, paste(path, "/sleeplog_", id, ".xlsx", sep=""))
+    
+    return('done, please check folder')
+    
+  }else {
+    return ("no sleep log generated: do not have at least one entry")
+ 
+  }
+  
+}
+  
+  
+# library(dplyr)
+# yuxi_sleeplog(path = '/Users/phoebelam/Desktop/yuxi',
+#               id = 101, 
+#               first = '2024-02-19')
+
